bace45c53f3653a5f95f648dfad4abfe1a2b0e6d
boyfeiyu <liufeiyu2000@qq.com>
2024-09-28 21:53:16 +0800

    initial: 初始化vue项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+const a = 1
+</script>
+
+<template>
+  <div class="hello-world">
+    <h2>hello-world</h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.bgc {
+  background-color: #f00;
+}
+</style>
