464fa1fea0fc5698e31e2e8c2875a4fcb3d3a8d0
boyfeiyu <liufeiyu2000@qq.com>
2024-10-07 18:15:38 +0800

    feat: 对接后端接口，完成图片的上传与展示示例

---
+++ src/components/MemeCraft.vue
@@ -0,0 +1,105 @@
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+
+const imageSources = ref<string[]>([])
+
+import api from '../utils/api'
+
+const imagePath = ref<string>('')
+const images = ref<File[]>([])
+const texts = ref<string[]>([''])
+const status = ref<string>('')
+
+const imageSrc = ref<string>('')
+
+const handleImageChange = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  if (target.files) {
+    images.value = Array.from(target.files)
+  }
+}
+
+const addTextField = () => {
+  texts.value.push('')
+}
+
+const handleSubmit = async () => {
+  status.value = '上传中...'
+
+  const formData = new FormData()
+  images.value.forEach((image) => {
+    formData.append('images', image)
+  })
+  formData.append(
+    'texts',
+    JSON.stringify(texts.value.filter((text) => text.trim() !== '')),
+  )
+
+  try {
+    const response = await api.post('/memes/test', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+      responseType: 'blob',
+    })
+    // response.data是一个图片的二进制数据，将其在页面上显示
+    imageSrc.value = URL.createObjectURL(new Blob([response.data]))
+    status.value = '上传成功！'
+    console.log(response.data)
+  } catch (error) {
+    status.value = '上传失败。'
+    console.error('错误:', error)
+  }
+}
+
+onMounted(async () => {})
+</script>
+
+<template>
+  <div class="hello-world">
+    <h2>hello-world1</h2>
+    <h2>meme 表情包生成器</h2>
+    <img
+      v-for="(src, index) in imageSources"
+      :key="index"
+      :src="src"
+      alt="Generated image"
+    />
+    <img :src="imagePath" alt="" />
+
+    <form @submit.prevent="handleSubmit" class="upload-form">
+      <div>
+        <label for="image-upload">上传图片：</label>
+        <input
+          type="file"
+          id="image-upload"
+          multiple
+          @change="handleImageChange"
+          accept="image/*"
+        />
+      </div>
+      <div v-for="(text, index) in texts" :key="index">
+        <input
+          type="text"
+          v-model="texts[index]"
+          :placeholder="`文本 ${index + 1}`"
+        />
+      </div>
+      <button type="button" @click="addTextField">添加文本字段</button>
+      <button type="submit">上传</button>
+      <p v-if="status">{{ status }}</p>
+    </form>
+    <div>
+      结果：
+      <img :src="imageSrc" alt="" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.bgc {
+  background-color: #f00;
+}
+</style>
+
+vue
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // 目标接口的域名
+        changeOrigin: true, // 是否改变源地址
+        rewrite: (path) => path.replace(/^\/api/, ''), // 重写接口路径
+      },
+    },
+  },
 })
+
+// http://localhost:3000
